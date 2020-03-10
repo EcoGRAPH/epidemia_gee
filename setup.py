6b3d007c38cef7d7f0c15bcc725713e7598b2f9c
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-03-10 17:44:18 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Measurements',
+    url='',
+    author='Ram',
+    author_email='Ramcr@ou.edu',
+    # Needed to actually package something
+    packages=['Sample'],
+    # Needed for dependencies
+    install_requires=['ee'],
+    install_requires=['wget'],
+    install_requires=['subprocess'],
+    install_requires=['click._compat'],
+    install_requires=['datetime'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
