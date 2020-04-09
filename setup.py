a820f5569dc26afd25b0dec8fa571acf9547dfb9
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-04-09 15:31:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['Ethiopia'],
     # Needed for dependencies
-    install_requires=['wget','datetime','requests'],
+    install_requires=['wget','datetime','requests','urllib'],
   #  install_requires=['wget'],
   #  install_requires=['subprocess'],
   #  install_requires=['click._compat'],
