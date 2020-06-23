f981d611834b5f34232717ee7c0afcda5b40c7ef
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-06-23 12:30:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,9 @@ setup(
     packages=['Ethiopia'],
     # Needed for dependencies
     install_requires=['wget','datetime','requests'],
-  #  install_requires=['wget'],
-  #  install_requires=['subprocess'],
-  #  install_requires=['click._compat'],
-   # install_requires=['datetime'],
-    # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
-    license='MIT',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    license='GPL-3',
+    description='This package helps in getting required environmental data. This package upon executing required functions will directly store data into Google-Drive ',
+    
 )
